3a280e97035de9ec82bf85f47cb1cfe248548760
jonifallon <joni.fallon@libertymutual.com>
2017-04-27 14:42:53 -0400

    Change to routes file
    For creating issues.  Still not resolved.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :issues, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
   post '/sign-up' => 'users#signup'
+  post '/create-issue' => 'issues#create'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
